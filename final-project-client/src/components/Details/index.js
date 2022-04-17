b7f0e68db5958175cfe086f2c9bda5f3bf671872
William Leith <williamleith@Williams-MacBook-Pro.local>
2022-04-17 15:04:07 -0400

    feat: first draft of create review functionality

--- final-project-client/src/components/Details/index.js
+++ final-project-client/src/components/Details/index.js
@@ -1,23 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import axios from "axios";
 import NavigationSidebar from "../NavigationSidebar";
 
 const Details = () => {
-    const [movieDetails, setMovieDetails] = useState({})
-    const [ourMovieDetails, setOurMovieDetails] = useState({})
+    const [movieDetails, setMovieDetails] = useState({});
+    const [ourMovieDetails, setOurMovieDetails] = useState([{}]);
+    const [newReview, setNewReview] = useState({});
     const url = "http://www.omdbapi.com/?apikey=de4824f3"
     const {imdbID} = useParams()
     const searchMovieByImdbID = async () => {
         const response = await axios.get(`${url}&i=${imdbID}`)
         setMovieDetails(response.data)
-        console.log('response');
-        console.log(response.data);
     }
     const searchOurMovieByImdbID = async () => {
-        const response = await axios.get(`http://localhost:4000/api/movies/${imdbID}`)
+        const response = await axios.get(`http://localhost:4000/api/reviews?imdbId=${imdbID}`)
         setOurMovieDetails(response.data)
     }
+
+    const createReview = async(review) => {
+        console.log('POST http://localhost:4000/api/reviews')
+        console.log('body: ', review);
+        const response = await axios.post('http://localhost:4000/api/reviews', review);
+        console.log('post response: ', response);
+    }
+
     useEffect(() => {
         searchMovieByImdbID()
         searchOurMovieByImdbID()
@@ -60,7 +67,71 @@ const Details = () => {
                     <span><b>Stars</b> {movieDetails.Actors}</span>
                 </div>
 
+                <hr/>
+
+                <div className="row">
+
+                    <h4>Add a Review</h4>
+
+                    <hr/>
+
+                    <textarea className="form-control"
+                        onChange={(e) =>
+                            setNewReview({...newReview,
+                                "username": "test_user",
+                                "imdbId": `${imdbID}`,
+                                "title": movieDetails.Title,
+                                "poster": movieDetails.Poster,
+                                "review": e.target.value
+                            })}
+                    >
+                    </textarea>
+
+                    <hr/>
+
+
+
+                    <button
+                        className = "btn btn-primary btn-block rounded-pill"
+                        onClick={() => createReview(newReview)}
+                    >
+                        Submit
+                    </button>
+
+                </div>
+
+
+                <hr/>
+
+                <h4>Recent Reviews</h4>
+
+                <hr/>
+
+                <div>
+                    {ourMovieDetails.map(
+                        movie =>
+                            <div className="row mt-2" key={movie._id || 1}>
+
+                                <div className="col-12">
+
+
+                                    <div>
+                                        <b>{movie.username}</b>
+                                    </div>
+
+                                    {movie.review}
+
+
+                                    <hr/>
+                                </div>
+                            </div>
+                    )}
+                </div>
+
+
+
             </div>
+
         </div>
     );
 };
